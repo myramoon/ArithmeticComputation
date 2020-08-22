3aed014556d6c97e2a892046db901509ad79732b
anamfazal <anamfazal94@gmail.com>
2020-08-22 12:06:53 +0530

    Adds code to perform arithmetic operation

--- arithmetic_Computation_Builder.sh
+++ arithmetic_Computation_Builder.sh
@@ -3,3 +3,7 @@
 read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c 
+
+result=$(( a + b * c ))		
+
+echo "Result of a+b*c is : " $result  #Print result
