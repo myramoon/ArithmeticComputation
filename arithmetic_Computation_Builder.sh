bf5ee86f20f075aa6a155b21542de6f556b26cf9
anamfazal <anamfazal94@gmail.com>
2020-08-22 13:53:35 +0530

    Adds code to sort the result array in descending order

--- arithmetic_Computation_Builder.sh
+++ arithmetic_Computation_Builder.sh
@@ -1,23 +1,45 @@
-#!/usr/local/bin/bash -x
+#!/usr/local/bin/bash 
 
+#DICTIONARY
 declare -A results
+
+#ARRAY
 declare -a array_result
 
-read -p "Enter first number : " a
+read -p "Enter first number : " a       #Take inputs from user
 read -p "Enter second number : " b
 read -p "Enter third number : " c 
 
 results["Result 1"]=$(( a + b * c ))	#Do each computation and store in dictionary		
 results["Result 2"]=$(( a * b + c ))	#Computation 2
-results["Result 3"]=$(( c + a / b ))   #Computation 3
-results["Result 4"]=$(( a % b + c ))   #Computation 4 
+results["Result 3"]=$(( c + a / b ))    #Computation 3
+results["Result 4"]=$(( a % b + c ))    #Computation 4 
 
-for (( count=0; count<=3; count++))
+for (( count=0; count<=3; count++))     #for loop to read dictionary's values into array
 do
 	
-   array_result[$count]=${results["Result $((count+1))"]} #read values in dictionary into array
+   array_result[$count]=${results["Result $((count+1))"]} 
 	
 done 
 
-echo ${array_result[@]}   #print array
+echo "Unsorted array of results : " ${array_result[@]}   #print unsorted array
+
+
+
+for (( j=0; j<=2; j++ ))	        #for loop to sort the array's contents
+do
+	for (( i=0; i<=2; i++ ))
+	do	
+		if [[ ${array_result[i]} -lt ${array_result[$((i+1))]} ]]
+		then
+			temp=${array_result[i]}    
+			array_result[i]=${array_result[$((i+1))]}
+			array_result[$((i+1))]=$temp
+		fi 
+
+	done
+done
+
+echo "Results in descending order : " ${array_result[@]}  # Print sorted array
+
 
