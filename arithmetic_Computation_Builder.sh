43271aa9bee2dc9624fb1305074f48bc45a818a5
anamfazal <anamfazal94@gmail.com>
2020-08-22 13:04:41 +0530

    Adds code to store dictionary's elements into an array

--- arithmetic_Computation_Builder.sh
+++ arithmetic_Computation_Builder.sh
@@ -1,6 +1,7 @@
-#!/usr/local/bin/bash 
+#!/usr/local/bin/bash -x
 
 declare -A results
+declare -a array_result
 
 read -p "Enter first number : " a
 read -p "Enter second number : " b
@@ -8,9 +9,15 @@ read -p "Enter third number : " c
 
 results["Result 1"]=$(( a + b * c ))	#Do each computation and store in dictionary		
 results["Result 2"]=$(( a * b + c ))	#Computation 2
-results["Result 3"]=$(( c + a / b ))    #Computation 3
-results["Result 4"]=$(( a % b + c ))    #Computation 4 
+results["Result 3"]=$(( c + a / b ))   #Computation 3
+results["Result 4"]=$(( a % b + c ))   #Computation 4 
 
-echo ${!results[@]}
-echo ${results[@]}
- 
+for (( count=0; count<=3; count++))
+do
+	
+   array_result[$count]=${results["Result $((count+1))"]} #read values in dictionary into array
+	
+done 
+
+echo ${array_result[@]}   #print array
+
