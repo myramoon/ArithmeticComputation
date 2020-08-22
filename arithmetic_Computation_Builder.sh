a94c43e5ceaca099d09ad77bde43dc8753965613
anamfazal <anamfazal94@gmail.com>
2020-08-22 12:51:35 +0530

    Refactors code to store results in a dictionary

--- arithmetic_Computation_Builder.sh
+++ arithmetic_Computation_Builder.sh
@@ -1,13 +1,16 @@
-#!/usr/local/bin/bash -x
+#!/usr/local/bin/bash 
+
+declare -A results
 
 read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c 
 
-result_1=$(( a + b * c ))	#Computation 1		
-result_2=$(( a * b + c ))	#Computation 2
-result_3=$(( c + a / b ))       #Computation 3
-result_4=$(( a % b + c ))       #Computation 4
+results["Result 1"]=$(( a + b * c ))	#Do each computation and store in dictionary		
+results["Result 2"]=$(( a * b + c ))	#Computation 2
+results["Result 3"]=$(( c + a / b ))    #Computation 3
+results["Result 4"]=$(( a % b + c ))    #Computation 4 
 
-echo $result_1 $result_2 $result_3 $result_4
+echo ${!results[@]}
+echo ${results[@]}
  
